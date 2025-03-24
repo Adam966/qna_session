d637af018a758d20cbe7cfc3bfa83ca124c24c00
Adam Ivan <43435815+Adam966@users.noreply.github.com>
2025-03-24 19:25:40 +0100

    Initial commit

---
+++ apps/qna_session_web/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import NxWelcome from '../app/NxWelcome.vue';
+</script>
+
+<template>
+  <main>
+    <NxWelcome title="@qna-session/qna_session_web" />
+  </main>
+</template>
